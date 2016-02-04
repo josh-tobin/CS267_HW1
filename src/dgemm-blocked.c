d7a58c4296f87e1a31bc42e1610e257349da6089
maxim-rabinovich <rabinovich@berkeley.edu>
2016-02-04 12:09:07 -0800

    Cleaned up.

--- src/dgemm-blocked.c
+++ src/dgemm-blocked.c
@@ -9,7 +9,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+static void do_block (int lda, int M, int N, int K, double* A, double* B, double* restrict C)
 {
   /* For each row i of A */
   for (int i = 0; i < M; ++i)
@@ -28,12 +28,12 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */  
-void square_dgemm (int lda, double* A, double* B, double* C)
+void square_dgemm (int lda, double* A, double* B, double* restrict C)
 {
   /* For each block-row of A */ 
-  for (int i = 0; i < lda; i += BLOCK_SIZE)
+  for (int i = 0; i < lda; i += BLOCK_SIZE) {
     /* For each block-column of B */
-    for (int j = 0; j < lda; j += BLOCK_SIZE)
+    for (int j = 0; j < lda; j += BLOCK_SIZE) {
       /* Accumulate block dgemms into block of C */
       for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
@@ -45,4 +45,6 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 	/* Perform individual block dgemm */
 	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
       }
+    }
+  }
 }
