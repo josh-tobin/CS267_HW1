1b1ae98fd5a71a059d9233c140756a9b5c7da8ec
maxim-rabinovich <rabinovich@berkeley.edu>
2016-02-07 14:53:53 -0800

    Successfully vectorized inner loop.

--- src/dgemm-blocked.c
+++ src/dgemm-blocked.c
@@ -9,26 +9,29 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* restrict C)
+static void do_block (const int lda, const int M, const int N, const int K, const double* const A, const double* const B, double* const __restrict__ C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
+  for (int i = 0; i < N; ++i) {
     /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
+    for (int j = 0; j < M; ++j) 
     {
       /* Compute C(i,j) */
+      const double const *Bj = B + j*lda;
       double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
-	cij += A[i+k*lda] * B[k+j*lda];
+      int l = 0;
+      for (int k = 0; k < K; ++k, l+= lda)
+	cij += Bj[k] * A[i + l];
       C[i+j*lda] = cij;
     }
+  }
 }
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */  
-void square_dgemm (int lda, double* A, double* B, double* restrict C)
+void square_dgemm (const int lda, const double* const A, const double* const B, double* const __restrict__ C)
 {
   /* For each block-row of A */ 
   for (int i = 0; i < lda; i += BLOCK_SIZE) {
